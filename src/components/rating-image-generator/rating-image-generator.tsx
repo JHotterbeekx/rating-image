c0f78c279c19b4f239c4bfeec5c753b329fd6adf
John Hotterbeekx <john.online@hotterbeekx.nl>
2018-10-07 19:25:20 +0200

    Setup of StencilJS

---
+++ src/components/rating-image-generator/rating-image-generator.tsx
@@ -0,0 +1,10 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'rating-image-generator'
+})
+export class RatingImageGenerator {
+  render() {
+    return <div>Ratings will be here</div>;
+  }
+}
